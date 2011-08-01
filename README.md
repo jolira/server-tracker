3e456039d6ab8cafd24f89ac58ade220342839a2
Joachim Kainz <jfk@jolira.com>
2011-08-01 13:47:17 -0700

    initial version.

---
+++ README.md
@@ -0,0 +1,5 @@
+server-tracker
+================
+
+Runs a server that allows other servers to log configuration information and
+view this information on desktop and mobile browsers.
---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var mustachio = require('mustachio');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.register('.mustache', mustachio);
+    app.set('view engine', 'mustache');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function() {
+    app.use(express.errorHandler({
+        dumpExceptions : true,
+        showStack : true
+    }));
+});
+
+app.configure('production', function() {
+    app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title : 'AssistMeLive'
+    });
+});
+
+app.listen(3080);
+console.log("Server listening on port %d in %s mode", app.address().port, app.settings.env);
