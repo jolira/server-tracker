b552c25d92b78b341cdec292e1c45d81fd7745b7
Joachim Kainz <jfk@jolira.com>
2011-08-06 21:36:24 -0700

    now builk-inserting into the database.

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
+var util = require('util');
 var express = require('express');
 var mustachio = require('mustachio');
-var metricServer = require('./lib/metric-server');
+var MetricServer = require('./lib/metric-server');
 
-var app = module.exports = express.createServer();
+module.exports = function(listenPort, database, dbServer, dbPort) {
+    var metricServer = new MetricServer(database, dbServer, dbPort);
+    var app = module.exports = express.createServer();
 
-// Configuration
-app.configure(function() {
-    app.set('views', __dirname + '/views');
-    app.register('.mustache', mustachio);
-    app.set('view engine', 'mustache');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function() {
-    app.use(express.errorHandler({
-        dumpExceptions : true,
-        showStack : true
-    }));
-});
-
-app.configure('production', function() {
-    app.use(express.errorHandler());
-});
-
-// Routes
-app.post('/submit/metric', function(req, res) {
-    metricServer.postMetric(req, res);
-});
-
-app.get('/', function(req, res) {
-    res.render('index', {
-        title : 'Server Tracker'
+    // Configuration
+    app.configure(function() {
+        app.set('views', __dirname + '/views');
+        app.register('.mustache', mustachio);
+        app.set('view engine', 'mustache');
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(__dirname + '/public'));
     });
-});
 
-app.listen(3080);
-console.log("Server listening on port %d in %s mode", app.address().port, app.settings.env);
+    app.configure('development', function() {
+        app.use(express.errorHandler({
+            dumpExceptions : true,
+            showStack : true
+        }));
+    });
+
+    app.configure('production', function() {
+        app.use(express.errorHandler());
+    });
+
+    // Routes
+    app.post('/submit/metric', function(req, res) {
+        metricServer.postMetric(req, res);
+    });
+
+    app.get('/', function(req, res) {
+        res.render('index', {
+            title : 'Server Tracker'
+        });
+    });
+
+    app.listen(3080);
+
+    var address = app.address();
+
+    console.log("Server listening %s in %s mode", util.inspect(address), app.settings.env);
+};
