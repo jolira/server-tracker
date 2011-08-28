f688a629ff649250d23305caaa7f33e6af53adb8
Joachim Kainz <jfk@jolira.com>
2011-08-28 07:38:53 -0700

    now using a config module.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mustachio = require('mustachio');
+var configuration = require('./lib/config');
 var MetricServer = require('./lib/metric-server');
 var QueryServer = require('./lib/query-server');
 var Server = require('./lib/server');
@@ -10,26 +11,37 @@ module.exports = function(options) {
     var mongo = options.mongo ? options.mongo : {};
 
     openDB(mongo, function(db) {
-        var metricServer = new MetricServer(db);
-        var queryServer = new QueryServer(db);
+        configuration(db, function(config) {
+            var metricServer = new MetricServer(db);
+            var queryServer = new QueryServer(db);
+            // var metaServer = new MetaServer(db, [ scrapeJXM.collection,
+            // MetricServer.collection ]);
 
-        scrapeJXM(db, options.jmx);
+            if (config.jmx) {
+                config.jmx = {};
+            }
 
-        var server = new Server();
+            scrapeJXM.run(db, config);
 
-        // Routes
-        server.post('/submit/metric', function(req, res) {
-            metricServer.postMetric(req, res);
+            var server = new Server();
+
+            // Routes
+            server.post('/submit/metric', function(req, res) {
+                metricServer.postMetric(req, res);
+            });
+            server.post('/query*', function(req, res) {
+                queryServer.postQuery(req, res);
+            });
+            server.post('/meta*', function(req, res) {
+                metaServer.postQuery(req, res);
+            });
+
+            // Listen
+            var listenPort = options.listenPort ? options.listenPort : 3080;
+
+            console.info("Listening to port %d", listenPort);
+
+            server.listen(listenPort);
         });
-        server.post('/query*', function(req, res) {
-            queryServer.postQuery(req, res);
-        });
-
-        // Listen
-        var listenPort = options.listenPort ? options.listenPort : 3080;
-
-        console.info("Listening to port %d", listenPort);
-
-        server.listen(listenPort);
     });
 };
