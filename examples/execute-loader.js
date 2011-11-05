0d106754fb838ba95adc99380147314fdac7dffb
Joachim Kainz <jfk@jolira.com>
2011-11-04 23:48:44 -0700

    now correctly processing groupBy.

--- examples/execute-loader.js
+++ examples/execute-loader.js
@@ -11,11 +11,26 @@ require("../lib/aggregator-loader.js");
 function start(options) {
   emitter.emit("config", {
     entry : {
+      qualifier : {},
       collection : "jmx",
       interval : 60000,
       out : "jmx-stats",
       groupBy: { "url.host" : 1 },
-      keys: { "heap used" : "value.HeapMemoryUsage.used" }
+      keys: {
+        "heap-used" : "value.HeapMemoryUsage.used",
+        "heap-committed" : "value.HeapMemoryUsage.committed",
+        "non-heap-used" : "value.NonHeapMemoryUsage.used",
+        "non-heap-committed" : "value.NonHeapMemoryUsage.committed",
+        "free-mem" : "value.FreePhysicalMemorySize",
+        "free-swap" : "value.FreeSwapSpaceSize",
+        "open-files" : "value.OpenFileDescriptorCount",
+        "committed-mem" : "value.CommittedVirtualMemorySize",
+        "current-threads" : "value.currentThreadCount",
+        "busy-threads" : "value.currentThreadsBusy",
+        "thread-peak" : "value.PeakThreadCount",
+        "thread-count" : "value.ThreadCount",
+        "load" : "value.SystemLoadAverage"
+      }
     },
     mongo : options.mongo
   }, function() {
